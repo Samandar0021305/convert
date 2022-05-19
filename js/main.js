658a15939384f2da0ef5d66e0a68041df6d036f0
https://github.com/Samandar0021305 <samandar021325@gmail.com>
2022-05-19 09:56:51 +0500

    started

---
+++ js/main.js
@@ -0,0 +1,46 @@
+const textarea = document.querySelector("textarea"),
+speechBtn = document.querySelector('button'),
+voisceList = document.querySelector("select");
+
+let synth = speechSynthesis,
+isSpeaking = true
+
+function vocies(){
+for(let voice of synth.getVoices()){
+    console.log(voice)
+    let selected = voice.name === "Google US English" ? "selected" : ""
+    let option = `<option value="${voice.name}" ${selected}>${voice.name} (${voice.lang}) </option>`
+  voisceList.insertAdjacentHTML("beforeend",option)
+}
+}
+synth.addEventListener("voiceschanged",vocies)
+
+function textToSpeech(text){
+    let utternance = new SpeechSynthesisUtterance(text)
+    speechSynthesis.speak(utternance)
+    for(let voice of synth.getVoices()){
+        if(voice.name === voisceList.value)
+        utternance.voice = voice
+    }
+}
+
+speechBtn.addEventListener('click',e=>{
+    e.preventDefault()
+    if(textarea.value !==""){
+        if(!synth.speaking){
+            textToSpeech(textarea.value)
+        }
+      if(textarea.value.length > 80){
+          if(isSpeaking){
+              synth.resume();
+              isSpeaking = false
+              speechBtn.innerHTML = "Pause Speech"
+          }else{
+              synth.pause;
+              isSpeaking = true
+              speechBtn.innerHTML = "Reaume Speech"
+          }
+      }
+    }  
+})
+
